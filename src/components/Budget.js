88b94a4e07ffe4b14227e347077603142fde4644
Muhammad Hariss <112217564+NykeHorus@users.noreply.github.com>
2023-02-23 11:54:02 -0500

    modified

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,38 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const {state,budget} = useContext(AppContext);
+    const {remaining}=useContext(AppContext);
+    const { dispatch} = useContext(AppContext);
+    
+    if(budget>20000){
+        alert("Budget Value exceeding 20,000")
+        return{
+           ...state
+        };
+    }; 
+    
+    if(remaining===0){
+        alert("Budget is below spendings")
+        return{
+            ...state
+        };
+    };
+    const changeBudget = (val)=>{
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: val,
+        });
+    }
     return (
-        <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+        <div className='alert alert-secondary'>Budget £{
+            <input 
+            type ='number'
+            defaultValue= {budget}
+            required='required'
+            onChange= {event=> changeBudget(event.target.value)}>
+            </input>}
         </div>
     );
 };
